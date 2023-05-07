1cfa3fd9c2918308d74bcc39660c3ab3621874e2
VitaliyOl <oliferchuk@gmail.com>
2023-05-07 17:08:13 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,24 @@ import { Container } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contact');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleAddContact = contact => {
     if (this.state.contacts.find(item => item.name === contact.name)) {
@@ -22,7 +32,7 @@ export class App extends Component {
     }
 
     this.setState(prevState => {
-      console.log(this.state.contacts);
+      // console.log(this.state.contacts);
       return {
         contacts: [...prevState.contacts, contact],
       };
